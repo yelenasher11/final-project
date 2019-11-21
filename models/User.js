6af37866a92cc005140dd2666afe72541645cb3c
orel ezhakian <yelenasher11@gmail.com>
2019-11-21 19:55:31 +0200

    test

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+//the user Schema
+//the email is unique that means no 2 users with the same email address
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
